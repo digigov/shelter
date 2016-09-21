f45c6bd3b742503a09065fcf9a457e0a46965c96
YuTin Liu <yuting1987@gmail.com>
2016-09-21 10:48:17 +0800

    refactor(assist): add size assist

---
+++ src/assist/size.js
@@ -0,0 +1,28 @@
+/**
+ * @providesModule size
+ */
+
+import { Platform } from 'react-native';
+
+export default {
+  margin: 30,
+  maxWidth: 340,
+  font: {
+    title: 36,
+    label: 20,
+    text: 18,
+    note: 14,
+    bold: '600',
+    light: '200',
+  },
+  ...Platform.select({
+    ios: {
+      navbar: 44,
+      statusBar: 20,
+    },
+    android: {
+      navbar: 56,
+      statusBar: 0,
+    },
+  }),
+};
