1b984ed949cf91117a80b34cd0c2943f295e53f4
YuTin Liu <yuting1987@gmail.com>
2016-09-22 14:24:45 +0800

    feat(component): add NavigationView

--- src/component/index.js
+++ src/component/index.js
@@ -7,6 +7,7 @@ import IconButton from './IconButton/IconButton';
 import Item from './Item/Item';
 import Label from './Label/Label';
 import Margin from './Margin/Margin';
+import NavigationView from './NavigationView/NavigationView';
 import TabBar from './TabBar/TabBar';
 import Title from './Title/Title';
 import VictimIdKeyboard from './VictimIdKeyboard/VictimIdKeyboard';
@@ -17,6 +18,7 @@ export {
   Item,
   Label,
   Margin,
+  NavigationView,
   TabBar,
   Title,
   VictimIdKeyboard,
